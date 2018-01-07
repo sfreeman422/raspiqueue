7b0c07d3f9c70a6590504b02ef8c5edb59ffc0ce
Steven Freeman <sfreeman422@gmail.com>
2018-01-07 13:48:31 -0500

    Further adjustments to the sql seed. Had some invalid syntax/improper foreign key constraints

--- musicStreamSchema.sql
+++ musicStreamSchema.sql
@@ -9,20 +9,20 @@ CREATE TABLE users(
     PRIMARY KEY (userId)
 );
 
-CREATE TABLE rooms(
-	roomId BIGINT NOT NULL AUTO_INCREMENT,
-    roomName VARCHAR(50) NOT NULL UNIQUE,
-    adminUser VARCHAR(50) NOT NULL,
-    pass VARCHAR(150),
-    FOREIGN KEY (adminUser) REFERENCES users(userId),
-    PRIMARY KEY (roomId)
-);
-
 CREATE TABLE links (
 	linkId BIGINT NOT NULL AUTO_INCREMENT,
     linkName VARCHAR(300) NOT NULL,
     linkUrl VARCHAR(400) NOT NULL,
     PRIMARY KEY (linkId)
+);
+
+CREATE TABLE rooms(
+	roomId BIGINT NOT NULL AUTO_INCREMENT,
+    roomName VARCHAR(50) NOT NULL UNIQUE,
+    adminUser BIGINT,
+    pass VARCHAR(150),
+    PRIMARY KEY (roomId),
+    CONSTRAINT FK_adminUser FOREIGN KEY (adminUser) REFERENCES users(userId)
 );
 
 CREATE TABLE rooms_links (
@@ -32,7 +32,7 @@ CREATE TABLE rooms_links (
     played TINYINT(1),
     lastModified TIMESTAMP,
     CONSTRAINT room_link_pk PRIMARY KEY (linkId, roomId),
-    CONSTRAINT FK_link FOREIGN KEY (linkId) REFERENCES links.linkId,
-    CONSTRAINT FK_room FOREIGN KEY (roomId) REFERENCES rooms.roomId,
-	CONSTRAINT FK_user FOREIGN KEY (userId) REFERENCES users.userId
+    CONSTRAINT FK_link FOREIGN KEY (linkId) REFERENCES links(linkId),
+    CONSTRAINT FK_room FOREIGN KEY (roomId) REFERENCES rooms(roomId),
+	CONSTRAINT FK_user FOREIGN KEY (userId) REFERENCES users(userId)
 );
