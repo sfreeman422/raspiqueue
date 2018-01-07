c17e61b5d9b58f8166876d3ef583e85e2f05800d
Steven Freeman <sfreeman422@gmail.com>
2018-01-07 12:29:45 -0500

    Changed up the schema to allow for a junction table. This will allow our DB to be more normalized. Will still need to work on this as I am brushing up on SQL and database design as I go

--- musicStreamSchema.sql
+++ musicStreamSchema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE musicStream;
+
 use musicStream;
 
 CREATE TABLE users(
@@ -11,19 +13,26 @@ CREATE TABLE rooms(
 	roomId BIGINT NOT NULL AUTO_INCREMENT,
     roomName VARCHAR(50) NOT NULL UNIQUE,
     adminUser VARCHAR(50) NOT NULL,
+    pass VARCHAR(150),
     FOREIGN KEY (adminUser) REFERENCES users(userId),
     PRIMARY KEY (roomId)
 );
 
 CREATE TABLE links (
 	linkId BIGINT NOT NULL AUTO_INCREMENT,
-    linkName VARCHAR(100) NOT NULL,
+    linkName VARCHAR(300) NOT NULL,
     linkUrl VARCHAR(400) NOT NULL,
-    roomId BIGINT NOT NULL,
-    userId BIGINT NOT NULL,
+    PRIMARY KEY (linkId)
+);
+
+CREATE TABLE rooms_links (
+	linkId BIGINT,
+    roomId BIGINT,
+    userId BIGINT,
     played TINYINT(1),
     lastModified TIMESTAMP,
-    FOREIGN KEY (roomId) REFERENCES rooms(roomId),
-    FOREIGN KEY (userId) REFERENCES users(userId),
-    PRIMARY KEY (linkId)
+    CONSTRAINT room_link_pk PRIMARY KEY (linkId, roomId),
+    CONSTRAINT FK_link FOREIGN KEY (linkId) REFERENCES links.linkId,
+    CONSTRAINT FK_room FOREIGN KEY (roomId) REFERENCES rooms.roomId,
+	CONSTRAINT FK_user FOREIGN KEY (userId) REFERENCES users.userId
 );
--- src/Children/VideoContent/VideoContent.js
+++ src/Children/VideoContent/VideoContent.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
+let player;
+
 class VideoContent extends Component {
+  constructor(props) {
+    super(props);
+    this.onYouTubeIframeApiReady = this.onYouTubeIframeApiReady.bind(this);
+    this.onPlayerReady = this.onPlayerReady.bind(this);
+    this.onPlayerStateChange = this.onPlayerStateChange.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.queueArr[0].linkUrl !== 'youtube.com') {
+      this.onYouTubeIframeApiReady();
+    }
+  }
+  onYouTubeIframeApiReady() {
+    player = new window.YT.Player('ytplayer', {
+      height: '80%',
+      width: '100%',
+      videoId: this.props.queueArr[0].linkUrl,
+      events: {
+        onReady: this.onPlayerReady,
+        onStateChange: this.onPlayerStateChange,
+      },
+    });
+  }
+  onPlayerReady(event) {
+    console.log(event);
+    event.target.playVideo();
+  }
+  onPlayerStateChange(event) {
+    console.log('state change');
+    console.log(event);
+  }
   render() {
+    console.log(this.props.queueArr[0].linkUrl)
     return (
       <div className="video-content">
-        <iframe title="video" src={`https://www.youtube.com/embed/${this.props.queueArr[0].linkUrl}`} />
+        <div id="ytplayer" />
+        {/* <iframe title="video" src={`https://www.youtube.com/embed/${this.props.queueArr[0].linkUrl}?enablejsapi=1&autoplay=1&controls=0`} /> */}
         <button className="thumbsDownButton"><i className="far fa-thumbs-down" /></button>
         <button className="thumbsUpButton"><i className="far fa-thumbs-up" /></button>
       </div>
