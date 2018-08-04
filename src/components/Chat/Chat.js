4a3b5f432c2fbbbb098fc6bf4637f5fd35cc60ec
Steven Freeman <sfreeman422@gmail.com>
2018-08-04 11:33:34 -0400

    Changes to chat scroll behavior

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -11,16 +11,54 @@ class ConnectedChat extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isScrolling: false,
       message: '',
     };
     this.handleUserMessage = this.handleUserMessage.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementsByClassName('message-area')[0].addEventListener('scroll', this.handleScroll);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.messages !== this.props.messages) {
+      this.scrollToBottom();
+    }
+  }
+
+  componentWillUnmount() {
+    document.getElementsByClassName('message-area')[0].removeEventListener('scroll', this.handleScroll);
   }
 
   handleUserMessage(event) {
     this.setState({
       message: event.target.value,
     });
+  }
+
+  scrollToBottom() {
+    console.log(this.state.isScrolling);
+    if (!this.state.isScrolling) {
+      this.setState({ isScrolling: false });
+      const chatDiv = document.getElementsByClassName('message-area');
+      chatDiv[0].scrollTop = chatDiv[0].scrollHeight;
+    }
+  }
+
+  handleScroll() {
+    let oldScroll;
+    const chat = document.getElementsByClassName('message-area')[0];
+    console.log(oldScroll < chat.scrollTop);
+    if (oldScroll < chat.scrollTop) {
+      console.log('user is scrolling up');
+      this.setState({ isScrolling: true });
+      this.scrollToBottomTimeOut = setTimeout(this.scrollToBottom, 3000);
+      this.oldScroll = chat.scrollTop;
+    }
   }
 
   sendMessage(event) {
